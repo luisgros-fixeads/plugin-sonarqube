6878aef5348bdab9b468f0351b2fea419b0ec76d
Luís Grós <luis.gros@olx.com>
2016-11-12 21:03:02 +0000

    remove variable export and bump version

--- bin/variables.sh
+++ bin/variables.sh
@@ -1,4 +1,4 @@
-export SQ_SERVER_CONTAINER=$(athena.plugin.get_prefix_for_container_name)-server-$(athena.os.get_instance)
-export SQ_SERVER_DOCKERFILE=$(athena.plugin.get_plg_docker_dir)/server
-export SQ_SCANNER_CONTAINER=$(athena.plugin.get_prefix_for_container_name)-scanner-$(athena.os.get_instance)
-export SQ_SCANNER_DOCKERFILE=$(athena.plugin.get_plg_docker_dir)/scanner
+SQ_SERVER_CONTAINER=$(athena.plugin.get_prefix_for_container_name)-server-$(athena.os.get_instance)
+SQ_SERVER_DOCKERFILE=$(athena.plugin.get_plg_docker_dir)/server
+SQ_SCANNER_CONTAINER=$(athena.plugin.get_prefix_for_container_name)-scanner-$(athena.os.get_instance)
+SQ_SCANNER_DOCKERFILE=$(athena.plugin.get_plg_docker_dir)/scanner
